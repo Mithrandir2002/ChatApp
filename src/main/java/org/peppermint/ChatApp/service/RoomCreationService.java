c066ba61fcd2dea3567dd36ad53aa3e1e64755a6
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2025-02-28 02:31:04 +0700

    add an service to let user create a chat room, look so dump but it's the only solution i come up with, might be design a better solution in the near future

--- src/main/java/org/peppermint/ChatApp/service/RoomCreationService.java
+++ src/main/java/org/peppermint/ChatApp/service/RoomCreationService.java
@@ -1,2 +1,23 @@
-package org.peppermint.ChatApp.service;public class RoomCreationService {
+package org.peppermint.ChatApp.service;
+
+import jakarta.persistence.Entity;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.peppermint.ChatApp.dto.MemberDTO;
+import org.peppermint.ChatApp.model.ChatRoom;
+import org.peppermint.ChatApp.model.Member;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class RoomCreationService {
+    public final MemberService memberService;
+    public final ChatRoomService roomService;
+
+    @Transactional
+    public ChatRoom createRoomWithAdmin(ChatRoom room, String userId) {
+        ChatRoom chatRoom = roomService.createChatRoom(room, userId);
+        Member member = memberService.joinChatRoom(userId, chatRoom.getRoomCode(), "ADMIN");
+        return chatRoom;
+    }
 }
