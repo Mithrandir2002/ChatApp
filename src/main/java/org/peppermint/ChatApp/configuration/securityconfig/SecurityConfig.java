633ee4073630469ffb7e50a53bd8d5c58d055485
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2025-02-28 17:12:44 +0700

    create a filter chain to to manage coming requests before they reach the front controller

--- src/main/java/org/peppermint/ChatApp/configuration/securityconfig/SecurityConfig.java
+++ src/main/java/org/peppermint/ChatApp/configuration/securityconfig/SecurityConfig.java
@@ -1,21 +1,74 @@
-package org.peppermint.ChatApp.configuration;
+package org.peppermint.ChatApp.configuration.securityconfig;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.peppermint.ChatApp.configuration.securityconfig.filter.AuthenticationFilter;
+import org.peppermint.ChatApp.configuration.securityconfig.filter.ExceptionHandlerFilter;
+import org.peppermint.ChatApp.configuration.securityconfig.filter.JWTAuthenticationFilter;
+import org.peppermint.ChatApp.configuration.securityconfig.manager.CustomAuthenticationManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+@AllArgsConstructor
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private final CustomAuthenticationManager authenticationManager;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/auth/signin");
         httpSecurity
                 .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()))
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
                         .requestMatchers("/ws/**").permitAll()
-                        .requestMatchers("/auth/signup").permitAll()
-                        .requestMatchers("/api/**").authenticated())
-                .addFilterBefore()
+                        .requestMatchers("/auth/register").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().authenticated())
+                .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
+                .addFilter(authenticationFilter)
+                .addFilterAfter(new JWTAuthenticationFilter(Arrays.asList(
+                        new AntPathRequestMatcher("/auth/register"),
+                        new AntPathRequestMatcher("/ws/**")
+                )), AuthenticationFilter.class)
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+                corsConfiguration.setAllowedOrigins(Arrays.asList(
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173",
+                        "http://localhost:5174",
+                        "http://127.0.0.1:5174"
+                ));
+                corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                corsConfiguration.setAllowCredentials(true);
+                corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                corsConfiguration.setExposedHeaders(Arrays.asList(
+                        "Authorization"
+                ));
+                corsConfiguration.setMaxAge(3600L);
+                return corsConfiguration;
+            }
+        };
     }
 }
