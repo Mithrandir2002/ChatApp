66f36d78875bd139090631a1e962eeecf33cf503
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2025-02-28 17:06:43 +0700

    modify the register method, return a user dto and include a jwt token at the response header

--- src/main/java/org/peppermint/ChatApp/controller/UserController.java
+++ src/main/java/org/peppermint/ChatApp/controller/UserController.java
@@ -1,7 +1,12 @@
 package org.peppermint.ChatApp.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.peppermint.ChatApp.configuration.securityconfig.SecurityConstants;
 import org.peppermint.ChatApp.dto.UserDTO;
+import org.peppermint.ChatApp.model.Member;
 import org.peppermint.ChatApp.model.User;
 import org.peppermint.ChatApp.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -10,14 +15,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @RestController
 public class UserController {
     public UserService userService;
 
-    @PostMapping("/api/register")
-    public ResponseEntity<UserDTO> registerUser(@RequestBody User user) {
-        return new ResponseEntity<>(mapToUserDTO(userService.registerUser(user)), HttpStatus.CREATED);
+    @PostMapping("/auth/register")
+    public ResponseEntity<UserDTO> registerUser(@RequestBody User user, HttpServletResponse response) {
+        User createdUser = userService.registerUser(user);
+        String token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
+        response.setHeader(SecurityConstants.JWT_HEADER, SecurityConstants.BEARER + token);
+        UserDTO userDTO = mapToUserDTO(user);
+        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
 
     public UserDTO mapToUserDTO(User user) {
