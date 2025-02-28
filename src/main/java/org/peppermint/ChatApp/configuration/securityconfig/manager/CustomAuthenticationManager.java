4a893059fb31f9727e0073d86627f08e7a95327c
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2025-02-28 17:11:10 +0700

    This custom class serve as utility class to help authentication filter check if user is existed in our record

--- src/main/java/org/peppermint/ChatApp/configuration/securityconfig/manager/CustomAuthenticationManager.java
+++ src/main/java/org/peppermint/ChatApp/configuration/securityconfig/manager/CustomAuthenticationManager.java
@@ -1,2 +1,28 @@
-package org.peppermint.ChatApp.configuration.securityconfig.manager;public class CustomAuthenticationManager {
+package org.peppermint.ChatApp.configuration.securityconfig.manager;
+
+import lombok.AllArgsConstructor;
+import org.peppermint.ChatApp.model.User;
+import org.peppermint.ChatApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+public class CustomAuthenticationManager implements AuthenticationManager {
+    @Autowired
+    private UserService userService;
+    private BCryptPasswordEncoder encoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        User user = userService.findUserByUsername(authentication.getName());
+        if (!encoder.matches(authentication.getCredentials().toString(), user.getPassword())) throw new BadCredentialsException("You provided an incorrect password!");
+        return new UsernamePasswordAuthenticationToken(authentication.getName(), user.getPassword());
+    }
 }
