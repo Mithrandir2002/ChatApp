e064b935988893439347bd9efc34039819c6444c
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2025-03-08 20:54:42 +0700

    add find chat room by channel method

--- src/main/java/org/peppermint/ChatApp/repository/ChatRoomRepository.java
+++ src/main/java/org/peppermint/ChatApp/repository/ChatRoomRepository.java
@@ -1,5 +1,6 @@
 package org.peppermint.ChatApp.repository;
 
+import org.peppermint.ChatApp.model.Channel;
 import org.peppermint.ChatApp.model.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,7 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
 
     @Query("select r from ChatRoom r where r.name like %:name%")
     List<ChatRoom> findByRoomName(@Param("name") String name);
+
+    @Query("select r from ChatRoom r join r.channels c where c = :channel")
+    Optional<ChatRoom> findByChannel(@Param("channel")Channel channel);
 }
